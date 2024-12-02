28f670f458cab06f2c1e25ee8c82b8e02f1de414
dgabai50 <dgabai50@stuy.edu>
2024-12-02 14:50:05 -0500

    started work on shell input

--- shell.c
+++ shell.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include "parse.h"
+#include <unistd.h>
+#include <string.h>
+
+
 int main() {
-	
+	char buffer[500];
+	char * args[50];
+	while (1) {
+		sleep(1);
+		printf("$ ");
+		fgets(buffer, 499, stdin);
+		parse_args(buffer, args);
+		execvp(args[0], args);
+	}		
 }
