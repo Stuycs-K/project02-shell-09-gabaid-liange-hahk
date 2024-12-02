332134c3337bde7f8f8996f10d1f882efd0a2a9c
dgabai50 <dgabai50@stuy.edu>
2024-12-02 14:36:20 -0500

    copied in execvp lab

---
+++ parse.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <string.h>
+
+void parse_args(char * line, char ** arg_ary) {
+	int index = 0;
+	char * curr = line;
+	char * token;
+	while((token = strsep(&curr, " "))) {
+		arg_ary[index] = token;
+		index++;
+	}	
+}
