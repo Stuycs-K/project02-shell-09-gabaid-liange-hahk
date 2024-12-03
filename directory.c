9fa6ff0b69a1efed59ef54ce20a3a3eeb9983559
keith <khah50@stuy.edu>
2024-12-03 00:31:51 -0500

    worked on cd and tracking current dir

---
+++ directory.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int changeDirect(char *path){
+    if(path == NULL){
+        char *home = "placeholder";
+        return chdir(home);
+    }
+
+    return chdir(path); 
+}
+
+void prompt(){
+    char buff[PATH_MAX];
+    char *cwd = getcwd(buff, PATH_MAX);  
+    if(cwd != NULL){
+        char *home = "placeholder";
+        printf("%s$", cwd);
+    }
+    else{
+        perror("getcwd fail");
+    }
+}
