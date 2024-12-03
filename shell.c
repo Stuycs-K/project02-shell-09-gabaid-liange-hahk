2c2f36d200a20a6a03963f9b88ee3e52cc24917a
dgabai50 <dgabai50@stuy.edu>
2024-12-02 19:12:52 -0500

    made basic working exec and fork loop

--- shell.c
+++ shell.c
@@ -2,16 +2,31 @@
 #include "parse.h"
 #include <unistd.h>
 #include <string.h>
-
+#include <errno.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 int main() {
 	char buffer[500];
 	char * args[50];
-	while (1) {
-		sleep(1);
+	int running = 1;
+	pid_t execFork;
+	int status;
+
+	while (running) {
 		printf("$ ");
 		fgets(buffer, 499, stdin);
+		buffer[strcspn(buffer, "\n")] = '\0';
 		parse_args(buffer, args);
-		execvp(args[0], args);
-	}		
+		execFork = fork();
+		if (execFork < 0) {
+			perror("fork failed\n");
+			exit(1);
+		} else if (execFork == 0) {
+			execvp(args[0], args);
+		} else {
+			int childId = wait(&status);
+		}
+		
+	}	
 }
