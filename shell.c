7bdc39b37d048c12b813d7cc59ccf69f1fb2b170
keith <khah50@stuy.edu>
2024-12-08 00:25:44 -0500

    moved pipehandle up just to test. still working on pipes but will get it done by tomorrow

--- shell.c
+++ shell.c
@@ -35,12 +35,10 @@ int main() {
 			while ((token = strsep(&buff, ";"))) {
 				if (!strcmp(token, "\0")) continue;
 				struct parse_info info = parse_args(token, args);
+				if (info.pipe_idx != -1) pipeHandle(token);
 				if (info.rout_idx != -1) args[info.rout_idx] = NULL;
  				if (info.rin_idx != -1) args[info.rin_idx] = NULL;
- 				if (info.pipe_idx != -1) {
-					args[info.pipe_idx] = NULL;
-					pipeHandle(token);
-				} else {
+ 				else {
 					if (!strcmp(args[0], "cd")) {
 						if (args[1] != NULL) {
 							changeDirect(args[1]);
@@ -57,10 +55,12 @@ int main() {
 							if (info.rout_idx != -1) {
 								backup = dup(fileno(stdout));
 								redirOut(args[info.rout_idx + 1]);
+								args[info.rout_idx] = NULL;
 							}
 							if (info.rin_idx != -1) {
 								backup2 = dup(fileno(stdin));
 								redirIn(args[info.rin_idx + 1]);
+								args[info.rout_idx] = NULL;
 							}
 							int ret = execvp(args[0], args);
 							if (info.rout_idx != -1) undoOut(backup);
