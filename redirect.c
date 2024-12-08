4da1042570e9b6bbaeb2764901d5532593e43ea4
dgabai50 <dgabai50@stuy.edu>
2024-12-07 23:22:58 -0500

    added comments

--- redirect.c
+++ redirect.c
@@ -6,25 +6,47 @@
 #include "redirect.h"
 
 int redirIn(char * input) {
+	/*
+	Args: File name
+	Return: 0 if succesful; calls err() if  not
+	Use Case: Use to process '<' token; redirects stdin to a file
+	*/
 	int inp = open(input, O_RDONLY);
-	dup2(inp, fileno(stdin));	
+	if (inp == -1) err();
+	if (dup2(inp, fileno(stdin)) == -1) err();	
 	close(inp);	
 	return 0;
 }
 
 int undoIn(int save) {
-	dup2(save, fileno(stdin));
+	/*
+	Args: A saved file descriptor for stdin
+	Return: 0 if succesful; calls err() if not
+	Use Case: Used to process '<' token; undoes the redirect from redirIn()
+	*/
+	if (dup2(save, fileno(stdin)) == -1) err();
 	return 0;
 }
 
 int redirOut(char * target) {
+	/*
+	Args: File name
+	Return: 0 if successful; calls err() if not 
+	Use Case: Used to proccess '>' token; redirects stdout to a file
+	*/
 	int tar = open(target, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	dup2(tar, fileno(stdout));
+	if (tar == -1) err();
+	if (dup2(tar, fileno(stdout)) == -1) err();
 	close(tar);
 	return 0;
 }
 
 int undoOut(int save) {
-	dup2(save, fileno(stdout));
+	/*
+	Args: A saved file descriptor for stdout
+	Return: 0 if successful; calls err() if not
+	Use Case: Used to proccess '>' token; undoes the redirect from redirIn()
+	*/
+	if (dup2(save, fileno(stdout)) == -1) err();
 	return 0;
 }
